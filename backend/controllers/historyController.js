b9f19f4ff4c6d6ee5a1b545a5c444bd6fbaec1f3
Rohan Kumar <rohangupta5671@gmail.com>
2026-01-17 14:37:36 +0530

    added apihistory delete controllers

--- backend/controllers/historyController.js
+++ backend/controllers/historyController.js
@@ -26,3 +26,27 @@ export const getApiHistoryById = async (req, res) => {
   }
 };
 
+export const deleteApiHistoryById = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const recordId = req.params.id;
+    const record = await history.findOneAndDelete({ _id: recordId, userId });
+    if (!record) {
+      return res.status(404).json({ error: "Record not found" });
+    }
+    return res.status(200).json({ message: "Record deleted successfully" });
+  }catch (error) {
+    return res.status(500).json({ error: "Server error" });
+  }
+}
+
+
+export const clearApiHistory = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    await history.deleteMany({ userId });
+    return res.status(200).json({ message: "All history cleared successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "Server error" });
+  }
+};
